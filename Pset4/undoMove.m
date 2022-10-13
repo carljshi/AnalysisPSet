251621ef3aee9505a37c396a744dc718e53c470d
Carl Shi <jingcheng.shi@berkeley.edu>
2022-10-12 22:00:06 -0400

    init commit

--- Pset4/undoMove.m
+++ Pset4/undoMove.m
@@ -1,4 +1,5 @@
 function [sudokuMyAnswer,sudokuhistory] = undoMove(sudokuMyAnswer, sudokuhistory)
 sudokuMyAnswer(sudokuhistory(end,1), sudokuhistory(end,2)) = sudokuhistory(end,3);
 sudokuhistory = sudokuhistory(1:(end-1),:);
+displayBoard(sudokuMyAnswer)
 end
