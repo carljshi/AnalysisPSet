27e2d84e2ea945824b748beb9790cba5511695b3
Carl Shi <jingcheng.shi@berkeley.edu>
2022-10-12 22:02:47 -0400

    init commit

--- Pset4/checkBoard.m
+++ Pset4/checkBoard.m
@@ -1,9 +1,11 @@
-function [error, location] = checkBoard(sudokuMyAnswer,x,y)
+function [valid, location] = checkBoard(sudokuMyAnswer,x,y)
 location = [];
 [valid1, rowerrorloc] = checkRow(sudokuMyAnswer, x, y);
 [valid2, columnerrorloc] = checkColumn(sudokuMyAnswer, x, y);
 [valid3, blockerrorloc] = checkBlock(sudokuMyAnswer, x, y);
 
-error = valid1 && valid2 && valid3;
-location = [rowerrorloc;columnerrorloc;blockerrorloc;x y];
+valid = valid1 && valid2 && valid3;
+if ~valid
+    location = [rowerrorloc;columnerrorloc;blockerrorloc;x y];
+end
 end
